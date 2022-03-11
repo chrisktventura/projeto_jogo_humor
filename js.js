8781c61b881aefcdaa53c8e7f1335ac925a8ae29
Christopher Ventura <chrisventura@outlook.com.br>
2022-03-10 22:53:22 -0300

    .

--- js.js
+++ js.js
@@ -6,20 +6,20 @@ let elementoBtn = document.querySelector('#alterar')
 elementoBtn.addEventListener('click', () =>{
     
     if(elementoBtn.value == 'primeiro'){
-        elementoImg.src = './assets/img/naruto1.jpg'
+        elementoImg.src = '../assets/img/naruto1.jpg'
         elementoNome.innerText = 'Naruto Uzumaki '
         elementoSituacao.innerText = 'Normal'
         elementoBtn.value = 'segundo'
     } else if(elementoBtn.value == 'segundo') { 
 
-        elementoImg.src = './assets/img/naruto2.png' 
+        elementoImg.src = '../assets/img/naruto2.png' 
         elementoNome.innerText = 'Naruto uma cauda'
         elementoSituacao.innerText = 'Descontrolado' 
         elementoBtn.value = 'terceiro'  
     } else { 
-        elementoImg.src = './assets/img/naruto3.jpg' 
+        elementoImg.src = '../assets/img/naruto3.jpg' 
         elementoNome.innerText = 'Naruto Capiroto' 
         elementoSituacao.innerText = 'Chama o Yamato'
-        elementoBtn.value = 'primeiro' 
+        elementoBtn.value = 'primeiro'
     }
 })
