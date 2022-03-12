da2d51ed48dead345217d9b07a679ec50114d86c
Christopher Ventura <chrisventura@outlook.com.br>
2022-03-11 22:32:39 -0300

    finalizado

--- js.js
+++ js.js
@@ -1,11 +1,12 @@
 const elementoNome = document.getElementById("nome");
 const elementoSituacao = document.querySelector("#situacao");
 const elementoImg = document.querySelector("#imagem");
+
 let elementoBtn = document.querySelector("#alterar");
 
 elementoBtn.addEventListener("click", () => {
   if (elementoBtn.value == "primeiro") {
-    elementoImg.src = "./assets/img/naruto1.jpg";
+    elementoImg.src = "./assets/img/naruto1.png";
     elementoNome.innerText = "Naruto Uzumaki ";
     elementoSituacao.innerText = "Normal";
     document.body.style.backgroundImage = "url('./assets/img/bg.jpg')";
@@ -20,11 +21,17 @@ elementoBtn.addEventListener("click", () => {
     document
     
     elementoBtn.value = "terceiro";
-  } else {
-    elementoImg.src = "./assets/img/naruto3.jpg";
+  } else if (elementoBtn.value == "terceiro") {
+    elementoImg.src = "./assets/img/naruto3.png";
     elementoNome.innerText = "Naruto 4 caudas";
     elementoSituacao.innerText = "Chama o Yamato";
     document.body.style.backgroundImage = "url('./assets/img/bg3.jpg')";
+    elementoBtn.value = "quarto";
+  } else {
+    elementoImg.src = "./assets/img/naruto4.png";
+    elementoNome.innerText = "Naruto 6 caudas";
+    elementoSituacao.innerText = "É o fim do Pain";
+    document.body.style.backgroundImage = "url('./assets/img/bg4.jpg')";
     elementoBtn.value = "primeiro";
   }
 });
